ab31be07740bc94f026031724dcbbcda3e1c1ce7
swkra <swkratisgiannoutsos@gmail.com>
2025-01-25 12:08:52 +0100

    Finished exercise2

--- ergasia1/hello.erl
+++ ergasia1/hello.erl
@@ -1,65 +1,44 @@
 -module(hello).
--export([add/2, sub/2, mul/2, divi/2, eval/1,eval/2]).
+-export([add/2, sub/2, mul/2, divi/2, eval/1, eval/2]).
 
+% Basic arithmetic functions
 add(A, B) -> A + B.
 sub(A, B) -> A - B.
 mul(A, B) -> A * B.
 divi(A, B) -> A div B.
 
-%---------------------------------------------------------------
-%1o zitoumeno
+% Evaluate without variables
 eval({Fun, A, B}) when is_integer(A), is_integer(B) ->
     {ok, apply(hello, Fun, [A, B])};
-%3o zitoumeno
 eval({Fun, A, B}) when is_tuple(A) ->
-    {ok, ResultA} = eval(A),
-    eval({Fun, ResultA, B});
-eval({Fun, A, B}) when is_tuple(B) ->
-    {ok, ResultB} = eval(B),
-    eval({Fun, A, ResultB});
-%2o zitoumeno
-eval(_) ->
-    error.
-
-%--------------------------------------------------------
-eval(A, B) when is_map(B) ->
-    Size = maps:size(B),
-    case Size of
-        0 -> eval(A);
-        _ ->
-            case A of
-                {Fun, Arg1, Arg2} ->
-                    ResolvedArg1 =
-                        case is_tuple(Arg1) of
-                            true ->
-                                case eval(Arg1, B) of
-                                    {ok, EvaluatedArg1} -> EvaluatedArg1;
-                                    _ -> Arg1
-                                end;
-                            false ->
-                                case maps:is_key(Arg1, B) of
-                                    true -> maps:get(Arg1, B);
-                                    false -> Arg1
-                                end
-                        end,
-                    ResolvedArg2 =
-                        case is_tuple(Arg2) of
-                            true ->
-                                case eval(Arg2, B) of
-                                    {ok, EvaluatedArg2} -> EvaluatedArg2;
-                                    _ -> Arg2
-                                end;
-                            false ->
-                                case maps:is_key(Arg2, B) of
-                                    true -> maps:get(Arg2, B);
-                                    false -> Arg2
-                                end
-                        end,
-                    eval({Fun, ResolvedArg1, ResolvedArg2});
-                _ ->
-                    {error, variable_not_found}
-            end
+    case eval(A) of
+        {ok, ResA} -> eval({Fun, ResA, B});
+        Error -> Error
     end;
-eval(_, _) ->
-    {error, variable_not_found}.
+eval({Fun, A, B}) when is_tuple(B) ->
+    case eval(B) of
+        {ok, ResB} -> eval({Fun, A, ResB});
+        Error -> Error
+    end;
+eval(_) -> {error, variable_not_found}.
 
+% Evaluate with variables in a map
+eval({Fun, A, B}, Map) when is_map(Map) ->
+    ResolvedA = resolve(A, Map),
+    ResolvedB = resolve(B, Map),
+    case {ResolvedA, ResolvedB} of
+        {{ok, ValA}, {ok, ValB}} -> eval({Fun, ValA, ValB});
+        {{error, _} = Error, _} -> Error;
+        {_, {error, _} = Error} -> Error
+    end;
+eval(_, _) -> {error, variable_not_found}.
+
+% Helper function to resolve a value
+resolve(Value, Map) when is_tuple(Value) ->
+    eval(Value, Map);
+resolve(Value, Map) ->
+    case maps:is_key(Value, Map) of
+        true -> {ok, maps:get(Value, Map)};
+        false when is_integer(Value) -> {ok, Value};
+        false -> {error, variable_not_found}
+    end.
