cc8e82b384471365c5731efeaf5cbbc126196817
swkra <swkratisgiannoutsos@gmail.com>
2025-01-27 00:37:31 +0100

    3D is working, needs some minor improvements

--- ergasia1/hello.erl
+++ ergasia1/hello.erl
@@ -1,15 +1,13 @@
 -module(hello).
--export([add/2, sub/2, mul/2, divi/2, eval/1, eval/2,map/2,filter/2,split/2]).
+-export([add/2, sub/2, mul/2, divi/2, eval/1, eval/2,map/2,filter/2,split/2,groupby/2]).
 %-------------------------------1------------------------------------------
-% Basic arithmetic functions
 add(A, B) -> A + B.
 sub(A, B) -> A - B.
 mul(A, B) -> A * B.
 divi(A, B) -> A div B.
 
-% Evaluate without variables
 eval({Fun, A, B}) when is_integer(A), is_integer(B) ->
-    {ok, apply(hello, Fun, [A, B])};
+    {ok, Fun(A,B)};
 eval({Fun, A, B}) when is_tuple(A) ->
     case eval(A) of
         {ok, ResA} -> eval({Fun, ResA, B});
@@ -23,6 +21,12 @@ eval({Fun, A, B}) when is_tuple(B) ->
 eval(_) -> {error, variable_not_found}.
 
 %---------------------------------2----------------------------------------
+eval(_, Map) when not is_map(Map) -> 
+    {error, there_is_no_map};
+
+eval(Fun, _) when not is_function(Fun) -> 
+    {error, there_is_no_function};
+
 eval({Fun, A, B}, Map) when is_map(Map) ->
     ResolvedA = resolve(A, Map),
     ResolvedB = resolve(B, Map),
@@ -30,17 +34,16 @@ eval({Fun, A, B}, Map) when is_map(Map) ->
         {{ok, ValA}, {ok, ValB}} -> eval({Fun, ValA, ValB});
         {{error, _} = Error, _} -> Error;
         {_, {error, _} = Error} -> Error
-    end;
-eval(_, _) -> {error, variable_not_found}.
+    end.
 
 resolve(Value, Map) when is_tuple(Value) ->
     eval(Value, Map);
 resolve(Value, Map) ->
     case maps:is_key(Value, Map) of
         true -> {ok, maps:get(Value, Map)};
-        false when is_integer(Value) -> {ok, Value};
         false -> {error, variable_not_found}
     end.
+
 %--------------------------------3A-----------------------------------------
 map(Fun, _) when not is_function(Fun) ->
     {error, not_function};
@@ -86,3 +89,20 @@ split(Fun, [H|T]) ->
         true -> {[H | FalseList], TrueList};  
         false -> {FalseList, [H | TrueList]}  
     end.
+%--------------------------------3D-----------------------------------------
+groupby(Fun, _) when not is_function(Fun) ->
+    {error, not_function};
+
+groupby(_, L) when not is_list(L) ->
+    {error, not_list};
+
+groupby(Fun, List) ->
+    groupby(Fun, List, 1, #{negative => [], positive => [], zero => []}).
+
+groupby(_, [], _, Acc) ->
+    Acc; 
+
+groupby(Fun, [H | T], Index, Acc) ->
+    Key = Fun(H),
+    UpdatedAcc = maps:update_with(Key, fun(L) -> [Index | L] end, [Index], Acc),
+    groupby(Fun, T, Index + 1, UpdatedAcc).
