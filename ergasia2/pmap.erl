0f4e69698cbfb895528022e9adebebead3802262
swkra <swkratisgiannoutsos@gmail.com>
2025-02-02 21:07:17 +0100

    3.1,3.2 are working

--- ergasia2/pmap.erl
+++ ergasia2/pmap.erl
@@ -1,36 +1,22 @@
 -module(pmap).
--export([unordered/2, unordered/3, worker/3, collect_results/2, process_tasks/3]).
+-export([unordered/2, unordered/3, worker/2, collect_results/3]).
 
-% Unordered processing without worker limit
 unordered(Fun, List) ->
-    [spawn(?MODULE, worker, [self(), Fun, X]) || X <- List], % Spawn all workers
-    collect_results(length(List), []). % Collect results asynchronously in an unordered manner
+    unordered(Fun, List, length(List)). % Default to spawning length(List) workers
 
-% Unordered processing with a worker limit (MaxWorkers)
 unordered(Fun, List, MaxWorkers) ->
     ActualWorkers = min(length(List), MaxWorkers), % Ensure MaxWorkers does not exceed List length
-    process_tasks(Fun, List, ActualWorkers). % Process tasks with limited parallelism
+    PidList = [spawn(?MODULE, worker, [self(), Fun]) || _ <- lists:seq(1, ActualWorkers)], % Spawn limited workers
+    collect_results(List, PidList, []). % Start processing
 
-% Helper function to process the list with a limited number of workers
-process_tasks(_Fun, [], _MaxWorkers) -> 
-    collect_results(0, []); % No tasks, return an empty list
-
-process_tasks(Fun, List, MaxWorkers) ->
-    WorkersToSpawn = min(length(List), MaxWorkers), % Ensure we don’t split more than the available elements
-    {Batch, Remaining} = lists:split(WorkersToSpawn, List), % Safely split the list
-    [spawn(?MODULE, worker, [self(), Fun, X]) || X <- Batch], % Spawn WorkersToSpawn processes
-    Results = collect_results(length(Batch), []), % Collect results of these workers
-    Results ++ process_tasks(Fun, Remaining, MaxWorkers). % Process remaining items
-
-% Worker function: applies Fun to X and sends the result to the parent process
-worker(Parent, Fun, X) ->
-    Parent ! Fun(X).
-
-% Collects results from worker processes
-collect_results(0, Results) -> 
-    Results; % Return results once all have been received
-
-collect_results(N, Acc) ->
+worker(Parent, Fun) ->
     receive
-        Result -> collect_results(N - 1, [Result | Acc]) % Accumulate results in arbitrary order
+        X -> Parent ! Fun(X), worker(Parent, Fun) % Compute and continue
+    end.
+
+collect_results([], _, Results) -> Results; % When all tasks are processed, return results
+collect_results([H | T], [Pid | PidList], Acc) -> % Take a worker without keeping Rest
+    Pid ! H, % Send it a task
+    receive
+        Result -> collect_results(T, [Pid | PidList], [Result | Acc]) % Collect result, continue
     end.
