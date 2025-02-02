a14d4d149e4b20d2314c8459971399fb4e640842
swkra <swkratisgiannoutsos@gmail.com>
2025-02-02 21:00:14 +0100

    minor improvments

--- ergasia2/pmap.erl
+++ ergasia2/pmap.erl
@@ -1,16 +1,16 @@
 -module(pmap).
--export([unordered/2, worker/3]).
+-export([unordered/2, worker/3, collect_results/2]).
 
 unordered(Fun, List) ->
-    unordered(Fun, List, []).
-
-unordered(_Fun, [], Results) -> lists:reverse(Results);
-
-unordered(Fun, [H | T], Acc) ->
-    spawn(?MODULE, worker, [self(), Fun, H]),  
-    receive
-        Result -> unordered(Fun, T, [Result | Acc]) 
-    end.
+    [spawn(?MODULE, worker, [self(), Fun, X]) || X <- List], % Spawn all workers
+    collect_results(length(List), []). % Collect results asynchronously in an unordered manner
 
 worker(Parent, Fun, X) ->
-    Parent ! Fun(X).
+    Parent ! Fun(X). % Send the result to the parent process
+
+collect_results(0, Results) -> 
+    Results; % Return results when all are received
+collect_results(N, Acc) ->
+    receive
+        Result -> collect_results(N - 1, [Result | Acc]) % Accumulate results in arbitrary order
+    end.
