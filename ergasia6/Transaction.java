84ee65171e399b1d5793ddeb67931e8d155d509c
swkra <swkratisgiannoutsos@gmail.com>
2025-03-04 10:07:17 +0100

    Added locks to the transaction file

--- ergasia6/Transaction.java
+++ ergasia6/Transaction.java
@@ -1,26 +1,31 @@
-// Peter Idestam-Almquist, 2023-02-26.
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.locks.ReentrantLock;
 
 class Transaction implements Runnable {
-	private List<Operation> operations = new ArrayList<Operation>();
-	private boolean closed = false;
-	
-	void add(Operation operation) {
-		if (closed) return;
-		operations.add(operation);
-	}
-	
-	void close() {
-		closed = true;
-	}
-	
-	public void run() {
-		if (!closed) return;
-		
-		// Execute the operations.
-		for (Operation operation : operations) {
-			operation.run();
-		}
-	}
-}	
+    private final ConcurrentLinkedQueue<Operation> operations = new ConcurrentLinkedQueue<>();
+    private volatile boolean closed = false;
+    private final ReentrantLock lock = new ReentrantLock();
+
+    void add(Operation operation) {
+        lock.lock();
+        try {
+            if (closed) return;
+            operations.add(operation);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    void close() {
+        closed = true;
+    }
+
+    public void run() {
+        if (!closed) return;
+
+        // Execute the operations.
+        for (Operation operation : operations) {
+            operation.run();
+        }
+    }
+}
